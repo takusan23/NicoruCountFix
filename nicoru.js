c3ce216f1936044862c974e2d1cd17f1db7b5219
takusan23 <takumu1200@gmail.com>
2024-08-10 01:18:59 +0900

    帰ってきたニコニコの視聴ページにはニコる数が HTML 内に埋め込まれてないみたい。とりあえず window オブジェクトに触るために web_accessible_resources を追加

--- nicoru.js
+++ nicoru.js
@@ -1,12 +1,16 @@
-setInterval(function () {
+// @ts-check
 
-    //ニコる数が表示されるspanを取得する
-    const spanList = document.getElementsByClassName('NicoruCell-count')
-    for (let index = 0; index < spanList.length; index++) {
-        const span = spanList[index];
-        // ニコる数は　data-nicoru-count 属性に入っている。
-        const nicoruCount = span.getAttribute('data-nicoru-count')
-        span.innerHTML = nicoruCount
-    }
+// nicoru-intercept.js の JS コードを <script> で差し込んでロードする
+// コンテンツスクリプトでやろうとすると、視聴ページの window オブジェクトにアクセスできない
+//（拡張機能別に window オブジェクトが用意されていて、視聴ページの window オブジェクトは隔離されてアクセスできない）
 
- }, 10)
+// これの解決策として、<script> を差し込む方法がある。これなら視聴ページの window オブジェクトを書き換えることが出来る
+// 参考：https://stackoverflow.com/questions/9515704/
+// nicoru-intercept.js が正しいニコる数に書き換えるコードです。nicoru.js は nicoru-intercept.js のコードを視聴ページへ差し込むだけです。
+// manifest.json に登録が必要です。
+
+const interceptScriptElement = document.createElement('script')
+interceptScriptElement.setAttribute('type', 'text/javascript')
+interceptScriptElement.src = chrome.runtime.getURL('nicoru-intercept.js')
+interceptScriptElement.onload = () => { interceptScriptElement.remove() }
+document.documentElement.appendChild(interceptScriptElement)
